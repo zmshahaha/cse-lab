ec8028b476ba86a1e507a768ec91f29e54b22fbf
maosheng zhao <zmshahaha@gmail.com>
2022-07-31 19:16:18 +0800

    lab1 finished

--- chfs_client.h
+++ chfs_client.h
@@ -6,6 +6,7 @@
 #include "extent_client.h"
 #include <vector>
 
+#define MAX_FILE_NAME 100
 
 class chfs_client {
   extent_client *ec;
@@ -26,33 +27,38 @@ class chfs_client {
     unsigned long mtime;
     unsigned long ctime;
   };
-  struct dirent {
-    std::string name;
+  typedef struct dirent {
+    char name[MAX_FILE_NAME];
+    //std::string name;
     chfs_client::inum inum;
-  };
+  }dir_t;
 
  private:
   static std::string filename(inum);
   static inum n2i(std::string);
-
+  int append_parent_d(inum,const char *,inum);
+  
  public:
   chfs_client();
   chfs_client(std::string, std::string);
 
   bool isfile(inum);
   bool isdir(inum);
+  bool issymlink(inum inum);
 
   int getfile(inum, fileinfo &);
   int getdir(inum, dirinfo &);
 
   int setattr(inum, size_t);
-  int lookup(inum, const char *, bool &, inum &);
+  int lookup(inum, const char *, inum &);
   int create(inum, const char *, mode_t, inum &);
   int readdir(inum, std::list<dirent> &);
   int write(inum, size_t, off_t, const char *, size_t &);
   int read(inum, size_t, off_t, std::string &);
   int unlink(inum,const char *);
   int mkdir(inum , const char *, mode_t , inum &);
+  int mksymlink(inum , const char *, const char *, inum &);
+  int readlink(inum , std::string &);
   
   /** you may need to add symbolic link related methods here.*/
 };
